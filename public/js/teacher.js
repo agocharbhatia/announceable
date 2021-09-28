65e72e934f12d040bfc3f95af419cdb5683d70f1
Agochar Bhatia <agocharbhatia11@gmail.com>
2021-09-27 22:54:46 -0400

    Added Modal Form Data Retrieval

--- public/js/teacher.js
+++ public/js/teacher.js
@@ -1,4 +1,22 @@
+function logout() {
+        firebase.auth().signOut().then(() => {
+            window.location.replace("/login.html")
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+
+function firebasePush(input) {
+    //Stops Braking
+    if (!firebase.apps.length) {
+        firebase.initializeApp(config);
+    }
+
+    //Push 
+}
+
 $(document).ready(function() {
+    //Check Auth
     firebase.auth().onAuthStateChanged((user) => {
         if (user) {
             console.log('logged in')
@@ -34,12 +52,40 @@ $(document).ready(function() {
 
     document.getElementById('logout-btn').addEventListener('click', logout);
 
-    function logout() {
-        firebase.auth().signOut().then(() => {
-            window.location.replace("/login.html")
-        }).catch((error) => {
-            console.log(error)
-        })
-    }
+    
+
+    $('#newAnnouncementForm').submit(function(event) {
+        var $form = $(this);
+        console.log('Submit to Firebase')
+
+        var title = $('#titleInput').val();
+
+        var message = $('#messageTextarea').val();
+
+        var club = $('#formClub').val();
+
+        var gr9 = $('#9Checkbox').prop('checked');
+        var gr10 = $('#10Checkbox').prop('checked');
+        var gr11 = $('#11Checkbox').prop('checked');
+        var gr12 = $('#12Checkbox').prop('checked');
+
+        var male = $('#maleFormCheckbox').prop('checked');
+        var female = $('#femaleFormCheckbox').prop('checked');
+
+        var newAnnouncement = {
+            "title": title,
+            "message": message,
+            "club": club,
+            "grade": {"9": gr9, "10": gr10, "11": gr11, "12": gr12},
+            "gender": {"male": male, "female": female}
+        }
+        
+        console.log(newAnnouncement)
+
+        var announcementsRef = firebase.database().ref('announcements');
+        var pushAnnouncement = 
+
+        event.preventDefault();
+    });
 });
 
